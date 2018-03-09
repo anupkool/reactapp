45feebadfda9bb16201c7a19107d8f10fa1df068
Anup Kulkarni <anup.sk@rediffmail.com>
2018-03-09 13:17:38 -0600

    adding css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots'; 
-
+import './App.css';
 class App extends Component {
 	constructor(){
 		super()
@@ -22,13 +22,12 @@ class App extends Component {
 		console.log(filteredRobots);
 		return (
 		<div className='tc'>
-		<h1>Robo Friends</h1>
+		<h1 className="f1">Robo Friends</h1>
 		<SearchBox searchChange={this.onSearchChange}/>
 		<CardList robots = {filteredRobots}/>
 		</div>
 		);	
 	}
-	
 }
 
 export default App;
